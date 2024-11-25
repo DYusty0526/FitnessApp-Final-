4134a53ec5156ac100992e0db6a2da68f60002f4
DYusty0526 <140427282+DYusty0526@users.noreply.github.com>
2024-11-24 18:39:53 -0700

    Add project files.

---
+++ FitnessApp/LoginForm.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace FitnessApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            InitializeLoginForm();
+        }
+
+        private void InitializeLoginForm()
+        {
+
+        }
+    }
+}
